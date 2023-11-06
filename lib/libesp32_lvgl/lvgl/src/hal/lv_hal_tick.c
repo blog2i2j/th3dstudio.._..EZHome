f4fcc2da55651f663e50f160a03b0dc2fcb2e71f
Timothy Hoogland <me@timothyhoogland.com>
2023-11-05 19:12:33 -0600

    13.2.0 Update - UNTESTED

--- lib/libesp32_lvgl/lvgl/src/hal/lv_hal_tick.c
+++ lib/libesp32_lvgl/lvgl/src/hal/lv_hal_tick.c
@@ -29,8 +29,8 @@
  *  STATIC VARIABLES
  **********************/
 #if !LV_TICK_CUSTOM
-static uint32_t sys_time = 0;
-static volatile uint8_t tick_irq_flag;
+    static uint32_t sys_time = 0;
+    static volatile uint8_t tick_irq_flag;
 #endif
 
 /**********************
@@ -46,7 +46,7 @@ static volatile uint8_t tick_irq_flag;
  * You have to call this function periodically
  * @param tick_period the call period of this function in milliseconds
  */
-LV_ATTRIBUTE_TICK_INC void lv_tick_inc(uint32_t tick_period)
+void LV_ATTRIBUTE_TICK_INC lv_tick_inc(uint32_t tick_period)
 {
     tick_irq_flag = 0;
     sys_time += tick_period;
